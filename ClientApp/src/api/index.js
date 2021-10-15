88a01cc5797cdb4adc5cfb1b0456b0e998288380
vladislavs.fedotovs <vladislavs.fedotovs@LV-LAS-SC-0073>
2021-10-15 15:35:15 +0300

    Index js update

--- ClientApp/src/api/index.js
+++ ClientApp/src/api/index.js
@@ -1,2 +1,2 @@
-export const root = `http://kudoshopbackend.azurewebsites.net/api`;
+export const root = `https://kudosshop.azurewebsites.net/api`;
 // export const root = `https://localhost:44386/api`;
