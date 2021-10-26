56207a62685d2cc935b043379ac10f5a5304efbe
Vladislavs Fedotovs <88765299+VFedot@users.noreply.github.com>
2021-10-26 14:47:44 +0300

    Update index.js

--- ClientApp/src/api/index.js
+++ ClientApp/src/api/index.js
@@ -1,2 +1,2 @@
-export const root = `https://kudosshop.azurewebsites.net/api`;
+export const root = `https://kudoshopbackend.azurewebsites.net/api`;
 // export const root = `https://localhost:44386/api`;
